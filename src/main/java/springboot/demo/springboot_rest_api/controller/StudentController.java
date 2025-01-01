181550dc90314eadbeddf91d8ad6dbefd14f7237
yuta asakura <yuta.asakura.se@gmail.com>
2025-01-01 20:47:01 +0900

    feat: add PUT endpoint to update student details
    - Added a new PUT endpoint (/students/{id}/update) in StudentController.
    - Accepts a Student object in the request body and the student ID as a path variable.
    - Simulates updating a student's details and returns the updated student object.
    - Maintains adherence to RESTful API design for handling updates.

--- src/main/java/springboot/demo/springboot_rest_api/controller/StudentController.java
+++ src/main/java/springboot/demo/springboot_rest_api/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -87,6 +88,20 @@ public class StudentController {
       System.out.println(student.getId());
       System.out.println(student.getFirstName());
       System.out.println(student.getLastName());
+      return student;
+    }
+
+    /**
+     * SpringBoot REST API - Request Body
+     * HTTP PUT request
+     * URL - http://localhost:8080/students/1/update
+     * @return
+     */
+    @PutMapping("/students/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+      System.out.println(student.getFirstName());
+      System.out.println(student.getLastName());
+
       return student;
     }
 }

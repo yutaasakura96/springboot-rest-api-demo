a7be3813a7dac92fd56bb9673b1f113bfa19b6c5
yuta asakura <yuta.asakura.se@gmail.com>
2025-01-01 20:52:54 +0900

    feat: add DELETE endpoint to remove a student by ID
    - Added a new DELETE endpoint (/students/{id}/delete) in StudentController.
    - Accepts a student ID as a path variable.
    - Simulates the deletion of a student and returns a success message.
    - Enhances API functionality for handling resource deletion.

--- src/main/java/springboot/demo/springboot_rest_api/controller/StudentController.java
+++ src/main/java/springboot/demo/springboot_rest_api/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -103,5 +104,17 @@ public class StudentController {
       System.out.println(student.getLastName());
 
       return student;
+    }
+
+    /**
+     * SpringBoot REST API - Path Variable
+     * HTTP DELETE request
+     * URL - http://localhost:8080/students/1/delete
+     * @return
+     */
+    @DeleteMapping("/students/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId) {
+      System.out.println(studentId);
+      return "Student deleted successfully";
     }
 }
